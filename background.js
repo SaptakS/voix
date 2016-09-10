f49a39ef5a1f77d19e1f9f4ff705747054a1be78
Saptak Sengupta <saptak013@gmail.com>
2016-09-11 05:02:15 +0530

    Add demo chrome extension

---
+++ background.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the user clicks on the browser action.
+  // No tabs or host permissions needed!
+chrome.tabs.onUpdated.addListener(
+    window.onload = function(tabId, changeInfo, tab) {
+      console.log('Turning ' + tab.url + ' red!');
+      chrome.tabs.executeScript({
+        code: 'document.body.style.backgroundColor="red"'
+      });
+});
+
+chrome.tabs.onActivated.addListener(
+    window.onload = function(activeInfo, tab) {
+      console.log('Turning ' + tab.url + ' red!');
+      chrome.tabs.executeScript({
+        code: 'document.body.style.backgroundColor="red"'
+      });
+});
+
+chrome.windows.onFocusChanged.addListener(
+    window.onload = function(windowId, tab) {
+      console.log('Turning ' + tab.url + ' red!');
+      chrome.tabs.executeScript({
+        code: 'document.body.style.backgroundColor="red"'
+      });
+});
+
+chrome.tabs.onCreated.addListener(
+    window.onload = function(tabId, tab) {
+      console.log('Turning ' + tab.url + ' red!');
+      chrome.tabs.executeScript({
+        code: 'document.body.style.backgroundColor="red"'
+      });
+});
